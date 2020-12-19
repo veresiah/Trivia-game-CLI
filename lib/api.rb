34ecab71ca9388b4d5ed8127ec575dd3ee4d824d
Veresia Hiralall <hiralallveresia@yahoo.com>
2020-12-19 10:19:25 -0500

    API get categories and question

---
+++ lib/api.rb
@@ -0,0 +1,26 @@
+require_relative "../config/environment.rb"
+class API
+    def self.categories 
+        resp = RestClient.get('https://opentdb.com/api_category.php')
+        hash = JSON.parse(resp.body,symbolize_names:true)
+        categories_array = hash[:trivia_categories]
+        categories_array.each do |category|
+            id = category[:id]
+            name = category[:name]
+            Category.new(id, name)
+        end 
+    end
+
+    def self.question
+        resp = RestClient.get('https://opentdb.com/api.php?amount=1&category=#{category.id}&type=boolean')
+        hash = JSON.parse(resp.body,symbolize_names:true)
+        quest_array = hash[:results]
+        category = question_array[0][:category] 
+        type = question_array[0][:type]
+        difficulty = question_array[0][:difficulty]
+        question = question_array[0][:question]
+        correct_answer = question_array[0][:correct_answer]
+        incorrect_answer = question_array[0][incorrect_answers]
+        Question.new(category, type, difficulty, question, correct_answer, incorrect_answer)
+    end 
+end 
