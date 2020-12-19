44a8298e5c1b167f1c502032786b7548f8aaa6ce
Veresia Hiralall <hiralallveresia@yahoo.com>
2020-12-19 09:28:05 -0500

    environment.rb updated

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require 'rest-client'
+require 'json'
+
+require_relative "../lib/api.rb"
+require_relative "../lib/category.rb"
+require_relative "../lib/question.rb"
+require_relative "../lib/cli.rb"
